b98ffe5ff29110768a6e7126703d4f69b8ccc532
pawan <pawan.gupta4@ibm.com>
2023-06-18 11:11:06 +0530

    after changes

--- main.tf
+++ main.tf
@@ -6,3 +6,17 @@ resource "aws_s3_bucket" "example" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
